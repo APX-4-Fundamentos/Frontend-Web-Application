9cc5590a1a6a71774d5947f5630566a682e9a2d8
sebastianrealcalderon <u20221d964@upc.edu.pe>
2025-09-16 18:04:36 -0500

    feat: auth page image, title and switcher

--- src/app/iam/pages/auth-page/auth-page.ts
+++ src/app/iam/pages/auth-page/auth-page.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
+import {MatButtonToggle, MatButtonToggleGroup} from '@angular/material/button-toggle';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-auth-page',
-  imports: [],
+  imports: [
+    NgOptimizedImage,
+    MatButtonToggleGroup,
+    MatButtonToggle,
+    FormsModule
+  ],
   templateUrl: './auth-page.html',
   standalone: true,
   styleUrl: './auth-page.css'
 })
 export class AuthPage {
-
+  authType: string = "login";
 }
